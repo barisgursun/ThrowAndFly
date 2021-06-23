98fc5c06de15ecfad8fa01d40afb3e448f14fb32
Baris Gursun <baris.gursun@hotmail.com>
2021-06-23 18:33:10 +0300

    karakterKontrol scripti oluşturuldu, kanat acma, kapama animasyonlari için parametreler verildi

---
+++ Assets/Scripts/karakterKontrol.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class karakterKontrol : MonoBehaviour
+{
+    Animator kontrol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        kontrol = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            Touch dokunus = Input.GetTouch(0);
+            if ((dokunus.phase == TouchPhase.Stationary) || (dokunus.phase == TouchPhase.Moved))
+            {
+                kontrol.SetBool("kanadiAcmayaBasla",true);
+                kontrol.SetBool("kanadiKapamayaBasla", false);
+            }
+
+            else if ((dokunus.phase == TouchPhase.Ended))
+            {
+                kontrol.SetBool("kanadiAcmayaBasla", false);
+                kontrol.SetBool("kanadiKapamayaBasla",true);
+            }
+
+       
+        }
+
+    }
+}
+
