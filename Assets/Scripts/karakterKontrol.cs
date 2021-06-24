2e7a1c71e31e73add41ec89b21bd5e4750149db4
Baris Gursun <baris.gursun@hotmail.com>
2021-06-24 14:51:57 +0300

    ekrana temas olunca cubugun surekli animasyona girmesi engellendi ve atis yapilmamisken ekrana temas oldugunda kanatların acilmasi engellendi

--- Assets/Scripts/karakterKontrol.cs
+++ Assets/Scripts/karakterKontrol.cs
@@ -4,33 +4,38 @@ using UnityEngine;
 
 public class karakterKontrol : MonoBehaviour
 {
-    Animator kontrol;
+    public Animator kontrol;
+    public Vector3 topkonum;
     // Start is called before the first frame update
     void Start()
     {
+
         kontrol = GetComponent<Animator>();
+        topkonum = gameObject.transform.position;
+        Debug.Log(topkonum);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.touchCount > 0)
+        if ((Input.touchCount > 0) && (topkonum != gameObject.transform.position))
         {
+
             Touch dokunus = Input.GetTouch(0);
             if ((dokunus.phase == TouchPhase.Stationary) || (dokunus.phase == TouchPhase.Moved))
             {
-                kontrol.SetBool("kanadiAcmayaBasla",true);
+                kontrol.SetBool("kanadiAcmayaBasla", true);
                 kontrol.SetBool("kanadiKapamayaBasla", false);
             }
 
             else if ((dokunus.phase == TouchPhase.Ended))
             {
                 kontrol.SetBool("kanadiAcmayaBasla", false);
-                kontrol.SetBool("kanadiKapamayaBasla",true);
+                kontrol.SetBool("kanadiKapamayaBasla", true);
             }
 
-       
+
         }
 
     }
---
+++ Assets/Scripts/stickKontrol.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stickKontrol : MonoBehaviour
+{
+    private karakterKontrol tophareketlimi;
+    public Animator cubukKontrol;
+    private Vector3 top;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tophareketlimi = GameObject.FindWithTag("ball").GetComponent<karakterKontrol>();
+        cubukKontrol = GetComponent<Animator>();
+        top = tophareketlimi.gameObject.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (top == tophareketlimi.gameObject.transform.position))
+        {
+
+            Touch dokunus = Input.GetTouch(0);
+            if ((dokunus.phase == TouchPhase.Stationary) || (dokunus.phase == TouchPhase.Moved))
+            {
+                cubukKontrol.SetBool("cekmeyeBasla", true);
+                cubukKontrol.SetBool("cekmeyiBirak", false);
+            }
+
+            else if ((dokunus.phase == TouchPhase.Ended))
+            {
+                cubukKontrol.SetBool("cekmeyeBasla", false);
+                cubukKontrol.SetBool("cekmeyiBirak", true);
+            }
+
+
+        }
+    }
+}
